34e36ae14ca1d9765b2dbb74d757405bf7057089
a0im <kzerozero1541@gmail.com>
2022-10-11 17:37:22 +0900

    1011

--- src/main.js
+++ src/main.js
@@ -6,17 +6,35 @@ const noticeData = notice.notice;
 // DOM
 const $header = document.querySelector("header");
 const $h_nav = document.querySelector(".logo-gnb-topset");
+const $h_eventWrap = document.querySelector(".event-wrap");
+const $h_gnbList = document.querySelector(".gnb-list");
+const $a_bgWrap = document.querySelector(".bg-wrap");
 const $topBtn = document.querySelector(".topBtn");
-const $eventWrap = document.querySelector(".event-wrap");
 const $noticeBox = document.querySelector(".notice-bundle");
 const $s2_itemList = document.querySelector(".s2-item-list");
 const $s2_items = document.querySelectorAll(".s2-item-list > li");
 const $s3 = document.querySelector(".s3");
 const $s3_imgBox = document.querySelector(".img-content");
 const $s3_content = document.querySelector(".text-content");
+const $a_textContent = document.querySelectorAll(".bg-wrap > .container > div");
 
+// ================ GNB- gnb slide menu ================
+gnbSildeEvnt();
+function gnbSildeEvnt() {
+  let [...gnbMenu] = $h_gnbList.children;
+  gnbMenu.forEach(menu => {
+    menu.addEventListener("mouseenter", () => {
+      $h_eventWrap.style.height = "250px";
+    });
+  });
+
+  $h_eventWrap.addEventListener("mouseleave", () => {
+    $h_eventWrap.style.height = "60px";
+  });
+}
 // ================= topBtn - 상단이동 , 버튼 숨김/표시 이벤트  ===================
-// 클릭-상단이동 리스너
+
+//evnt
 $topBtn.addEventListener("click", () => {
   setTimeout(() => {
     $h_nav.scrollIntoView({ behavior: "smooth" });
@@ -43,69 +61,97 @@ let option3 = {
 const observer_arrow = new IntersectionObserver(isEvt_arrow, option3);
 observer_arrow.observe($header);
 
-// ================= notice - 공지내용 일정 시간마다 위로 슬라이드 이벤트 ===================
-const eleSize = 60;
-let currIdx = 0;
-let interval = null;
+// ======================== Article -  change img article area ========================
 
-//함수 호출 묶음
-let coreFn = () => {
-  createNoticeEle();
-  const $notiEle = document.querySelectorAll(".notice-bundle > p");
-  createCloneNodes($notiEle);
-  interval = setInterval(() => moveNotice(currIdx++), 2500);
-  toggleInterval($notiEle);
-};
-coreFn();
+changeArticleImgFn();
+function changeArticleImgFn() {
+  let bg = ["../img/h-bg4.jpg", "../img/h-bg3.jpg"];
+  let bgIdx = 0;
+  let interval = null;
 
-//.notice-bundle내 요소 생성 함수
-function createNoticeEle() {
-  let element = "";
-  noticeData.map(data => {
-    element += `<p><a href="#">[${data.type.toUpperCase()}] ${data.title}<time>${data.date}</time><a/></p>`;
-  });
-
-  $noticeBox.innerHTML = element;
-}
-
-// 요소 복사 함수
-function createCloneNodes($notiEle) {
-  $notiEle.forEach(e => {
-    let clone = e.cloneNode(true);
-    clone.classList.add("clone");
-    $noticeBox.appendChild(clone);
-  });
-  $noticeBox.classList.add("noti-animate");
-}
-//요소 이동 함수
-function moveNotice(idx) {
-  let currenPos = idx * eleSize;
-  $noticeBox.style.bottom = `${currenPos}px`;
-  resetIdx();
-}
-
-//요소 위치 초기화 함수
-function resetIdx() {
-  const notiLength = document.querySelector(".notice-bundle").children.length / 4;
-
-  if (currIdx > notiLength) {
-    setTimeout(() => {
-      $noticeBox.classList.remove("noti-animate");
-      $noticeBox.style.bottom = "0px";
-      currIdx = 0;
-    }, 350);
-    setTimeout(() => $noticeBox.classList.add("noti-animate"), 401);
+  //초기화, 클래스 토글, 배경 변경 - 함수
+  function isEvnt() {
+    if (bgIdx >= bg.length) bgIdx = 0;
+    $a_bgWrap.style.backgroundImage = `url('${bg[bgIdx]}')`;
+    $a_textContent.forEach(e => e.classList.remove("act-bg"));
+    $a_textContent[bgIdx].classList.add("act-bg");
+    bgIdx++;
   }
+
+  interval = setInterval(() => isEvnt(), 3500);
+  //toggle interval 함수
+  $a_bgWrap.addEventListener("mouseenter", () => clearInterval(interval));
+  $a_bgWrap.addEventListener("mouseleave", () => (interval = setInterval(() => isEvnt(), 3500)));
 }
 
-//toggle interval 함수
-function toggleInterval($notiEle) {
-  $notiEle.forEach(pTag => {
-    pTag.children[0].addEventListener("mouseenter", () => clearInterval(interval));
-    pTag.children[0].addEventListener("mouseleave", () => {
-      interval = setInterval(() => moveNotice(currIdx++), 2500);
+// ================= notice - 공지내용 일정 시간마다 위로 슬라이드 이벤트 ===================
+
+slideNoticeFn();
+function slideNoticeFn() {
+  const eleSize = 60;
+  let currIdx = 0;
+  let interval = null;
+
+  //함수 호출 묶음
+  let coreFn = () => {
+    createNoticeEle();
+    const $notiEle = document.querySelectorAll(".notice-bundle > p");
+    createCloneNodes($notiEle);
+    interval = setInterval(() => moveNotice(currIdx++), 2500);
+    toggleInterval($notiEle);
+  };
+  coreFn();
+
+  //.notice-bundle내 요소 생성 함수
+  function createNoticeEle() {
+    let element = "";
+    noticeData.map(data => {
+      element += `<p><a href="#">[${data.type.toUpperCase()}] ${data.title}<time>${data.date}</time><a/></p>`;
     });
-  });
+
+    $noticeBox.innerHTML = element;
+  }
+
+  // 요소 복사 함수
+  function createCloneNodes($notiEle) {
+    $notiEle.forEach(e => {
+      let clone = e.cloneNode(true);
+      clone.classList.add("clone");
+      $noticeBox.appendChild(clone);
+    });
+
+    $noticeBox.classList.add("noti-animate");
+  }
+  //요소 이동 함수
+  function moveNotice(idx) {
+    let currenPos = idx * eleSize;
+    $noticeBox.style.bottom = `${currenPos}px`;
+    resetIdx();
+  }
+
+  //요소 위치 초기화 함수
+  function resetIdx() {
+    const notiLength = document.querySelector(".notice-bundle").children.length / 4;
+
+    if (currIdx > notiLength) {
+      setTimeout(() => {
+        $noticeBox.classList.remove("noti-animate");
+        $noticeBox.style.bottom = "0px";
+        currIdx = 0;
+      }, 350);
+      setTimeout(() => $noticeBox.classList.add("noti-animate"), 401);
+    }
+  }
+
+  //toggle interval 함수
+  function toggleInterval($notiEle) {
+    $notiEle.forEach(pTag => {
+      pTag.children[0].addEventListener("mouseenter", () => clearInterval(interval));
+      pTag.children[0].addEventListener("mouseleave", () => {
+        interval = setInterval(() => moveNotice(currIdx++), 2500);
+      });
+    });
+  }
 }
 
 // ============= s2 - 일정 시간마다 이미지 우측 이동 이벤트 ================
@@ -156,24 +202,6 @@ function s2SlideEvnt() {
   }
 }
 
-// ============= nav - nav영역 fixed 이벤트 ================
-// 스타일 변경 함수
-// let navAddEvt = () => ($eventWrap.style.transform = "scale(1.03)");
-// let navRemoveEvt = () => ($eventWrap.style.transform = "scale(1)");
-// let isEvt_nav = (entries, observer) => {
-//   !entries[0].isIntersecting ? navAddEvt() : navRemoveEvt();
-// };
-
-// //Observer option
-// let option2 = {
-//   root: null,
-//   rootMargin: "0px",
-//   threshold: 0.1,
-// };
-// //create observer obj
-// const observer_nav = new IntersectionObserver(isEvt_nav, option2);
-// observer_nav.observe($h_nav);
-
 // ============= s3 - 텍스트 슬라이드 이벤트   ================
 
 //class 추가 제거 함수
@@ -199,3 +227,21 @@ let option1 = {
 //create observer obj
 const observer_s3 = new IntersectionObserver(isEvt_s3, option1);
 observer_s3.observe($s3);
+
+// ============= nav - nav영역 fixed 이벤트 ================
+// 스타일 변경 함수
+// let navAddEvt = () => ($eventWrap.style.transform = "scale(1.03)");
+// let navRemoveEvt = () => ($eventWrap.style.transform = "scale(1)");
+// let isEvt_nav = (entries, observer) => {
+//   !entries[0].isIntersecting ? navAddEvt() : navRemoveEvt();
+// };
+
+// //Observer option
+// let option2 = {
+//   root: null,
+//   rootMargin: "0px",
+//   threshold: 0.1,
+// };
+// //create observer obj
+// const observer_nav = new IntersectionObserver(isEvt_nav, option2);
+// observer_nav.observe($h_nav);
